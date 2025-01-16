8b70ce826df1d438f2443255a166fab210f49fff
Admin <admin@MacBook-Pro.lan>
2025-01-16 20:02:05 +0200

    add an item

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def get_items():
     items = [
         {"id": 1, "name": "Item 1"},
-        {"id": 2, "name": "Item 2"}
+        {"id": 2, "name": "Item 2"},
+        {"id": 3, "name": "Item 3"}
     ]
     return jsonify(items)
 
