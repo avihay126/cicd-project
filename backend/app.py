c1e7fb07792311b407ab97abd3ae863d4912cb6a
Admin <admin@MacBook-Pro.lan>
2025-01-16 20:58:54 +0200

    new Item

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,8 @@ def get_items():
         {"id": 2, "name": "Item 2"},
         {"id": 3, "name": "Item 3"},
         {"id": 4, "name": "Item 4"},
-        {"id": 5, "name": "Item 5"}
+        {"id": 5, "name": "Item 5"},
+        {"id": 6, "name": "Item 5"}
     ]
     return jsonify(items)
 
