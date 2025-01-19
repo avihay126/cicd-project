16a2fee8b0fa56f0578ce41de0b21c3407773253
Admin <admin@MacBook-Pro.lan>
2025-01-19 13:39:58 +0200

    fix item6 id

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ def get_items():
         {"id": 3, "name": "Item 3"},
         {"id": 4, "name": "Item 4"},
         {"id": 5, "name": "Item 5"},
-        {"id": 6, "name": "Item 5"}
+        {"id": 6, "name": "Item 6"}
     ]
     return jsonify(items)
 
